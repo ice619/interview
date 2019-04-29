7893afa32373dbbbe822b58a3dc4499d265508d3
chenpeng <chenpeng@xiaoniu.com>
2019-04-29 19:36:04 +0800

    初始化

---
+++ src/main/java/com/cp/interview/InterviewApplication.java
@@ -0,0 +1,13 @@
+package com.cp.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+}
