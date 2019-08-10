148756a2cc0c381cd11e7240927daf3546d3d485
cp <ice619@126.com>
2019-08-10 20:48:36 +0800

    join()

--- src/main/java/com/cp/interview/thread/base/Thread1.java
+++ src/main/java/com/cp/interview/thread/base/Thread1.java
@@ -16,6 +16,12 @@ public class Thread1 {
         });
 
         thread.start();
+        try {
+            //主线程调用了hread.join()，表示主线程将执行权让给thread线程，等thread线程执行完之后，主线程才执行
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         for (int i = 1; i < 10; i++) {
             System.out.println("主线程 i：" + i);
